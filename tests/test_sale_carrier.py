79149e057004a54f2dd807629296a0705423c704
resteve <resteve@zikzakmedia.com>
2015-11-29 23:28:10 +0100

    Tests

--- tests/test_sale_carrier.py
+++ tests/test_sale_carrier.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
-#This file is part sale_carrier module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part of the sale_carrier module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleCarrierTestCase(unittest.TestCase):
-    '''Test Sale Carrier module'''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_carrier')
-
-    def test0005views(self):
-        '''Test views'''
-        test_view('sale_carrier')
-
-    def test0006depends(self):
-        '''Test depends'''
-        test_depends()
+class SaleCarrierTestCase(ModuleTestCase):
+    'Test Sale Carrier module'
+    module = 'sale_carrier'
 
 
 def suite():
