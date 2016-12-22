22897bf4fa120a67e21f37be60402ffad96ef5f1
Raimon Esteve <raimon@nan-tic.com>
2016-12-22 18:44:08 +0100

    Add carrier field in configuration

--- sale.py
+++ sale.py
@@ -1,5 +1,5 @@
 #This file is part sale_carrier module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool, PoolMeta
 from trytond.model import fields
@@ -14,6 +14,7 @@ class Sale:
     @fields.depends('party')
     def on_change_party(self):
         super(Sale, self).on_change_party()
+
         self.carrier = None
         if self.party:
             if self.party.carrier:
