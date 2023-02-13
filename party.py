b718ebaefc4b916eb5c6bc954561ad3f42f401d9
Raimon Esteve <raimonesteve@gmail.com>
2023-02-13 17:42:01 +0100

    Add available carriers from party

--- party.py
+++ party.py
@@ -1,12 +1,21 @@
 # This file is part sale_carrier module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.model import fields
+from trytond.model import fields, ModelSQL
 from trytond.pool import PoolMeta
-
-__all__ = ['Party']
 
 
 class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
     carrier = fields.Many2One('carrier', 'Carrier')
+    carriers = fields.Many2Many('party.party-carrier', 'party', 'carrier',
+        'Carriers')
+
+
+class PartyCarrier(ModelSQL):
+    'Party - Carrier'
+    __name__ = 'party.party-carrier'
+    party = fields.Many2One('party.party', 'Party', ondelete='CASCADE',
+        required=True, select=True)
+    carrier = fields.Many2One('carrier', 'Carrier', ondelete='CASCADE',
+        required=True, select=True)
